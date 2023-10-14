ca179ec0607d43b154a235351f389b0eb72b3512
saurabh.kumar <saurabh.kumar@revolut.com>
2023-10-14 17:28:39 +0100

    Javalin first

--- javalin-demo/src/main/java/org/saurabh/schedule/JavalinHtmlFormsExampleApp.java
+++ javalin-demo/src/main/java/org/saurabh/schedule/JavalinHtmlFormsExampleApp.java
@@ -3,9 +3,17 @@ package org.saurabh.schedule;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.util.FileUtil;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.SimpleEmail;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static j2html.TagCreator.br;
+import static j2html.TagCreator.button;
+import static j2html.TagCreator.form;
+import static j2html.TagCreator.input;
+import static j2html.TagCreator.textarea;
 
 public class JavalinHtmlFormsExampleApp {
 
@@ -18,9 +26,8 @@ public class JavalinHtmlFormsExampleApp {
 
     public static void main(String[] args) {
 
-        final var app = Javalin.create(config ->
-                config.staticFiles.add("/public", Location.CLASSPATH)
-            )
+        final var app = Javalin
+            .create(config -> config.staticFiles.add("/public", Location.CLASSPATH))
             .start(7070);
 
         app.post("/make-reservation", ctx -> {
@@ -38,5 +45,35 @@ public class JavalinHtmlFormsExampleApp {
             });
             ctx.html("Upload successful");
         });
+
+        app.get("/help", ctx -> ctx.html(
+            form()
+                .withAction("/contact-us")
+                .withMethod("post")
+                .with(
+                    input().withName("subject").withPlaceholder("Subject"),
+                    br(),
+                    textarea().withName("message").withPlaceholder("Your message ..."),
+                    br(),
+                    button("Submit")
+                ).render()
+        ));
+
+        app.post("/contact-us", ctx -> {
+            final var email = new SimpleEmail();
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator("<email>", "<password>"));
+            email.setSSLOnConnect(true);
+            email.setFrom("FROM");
+            email.setSubject(ctx.formParam("subject"));
+            email.setMsg(ctx.formParam("message"));
+            email.addTo("TO");
+            email.send(); // will throw email-exception if something is wrong
+            ctx.redirect("/contact-us/success");
+        });
+
+        app.get("/contact-us/success", ctx -> ctx.html("Your message was sent"));
+
     }
 }
