13440f0826cd69b9527aed42853d7ab358e7da43
saurabh.kumar <saurabh.kumar@revolut.com>
2023-10-14 12:00:09 +0100

    remove dumb service

--- department-api/src/main/java/com/saurabh/departmentapi/repositories/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.saurabh.departmentapi.repositories;
-
-import com.saurabh.departmentapi.entities.Department;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends CrudRepository<Department, Long> {
-}
